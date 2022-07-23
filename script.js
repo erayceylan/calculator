9f823960284473d547b4633a7d51455735248fb1
erayceylan <erayceylan@rocketmail.com>
2022-07-23 07:21:21 +0300

    Add repetitive operations and history

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function add(...number) {
-    return number.reduce((sum,number) => sum + number)
-}
-
-function substract(...number) {
-    return number.reduce((sum,number) => sum - number)
-}
-
-function multiply(...number) {
-    return number.reduce((sum,number) => sum * number)
-}
-
-function divide(...number) {
-    return number.reduce((sum,number) => sum / number)
-}
-
-window.addEventListener('keydown', (e) => captureEvent(e));
-
 function captureEvent(e) {
 
     const numbers = ["1","2","3","4","5","6","7","9","0"]
@@ -27,15 +9,64 @@ function captureEvent(e) {
     if(utilities.includes(e.key)) utilityPressed(e.key)
 }
 
-function updateDisplay(char) {
-    document.querySelector(".screen-main").innerHTML += char;
+function numberPressed(number) {
+
+    if (mainDisplay.innerHTML == "0") mainDisplay.innerHTML = ''
+
+    mainDisplay.innerHTML += number
+    calculation.screen = +mainDisplay.innerHTML
 }
 
-function numberPressed(number) {
-    updateDisplay(number)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function operatorPressed(operator) {
+
+    operatorHandler(operator)
+
+    if (calculation.number[0] == undefined) {
+        calculation.number[0] = calculation.screen
+        calculation.screen = undefined
+    }
+
+    if (calculation.number[1] == undefined) {
+        calculation.number[1] = calculation.screen
+        calculation.screen = undefined
+    }
+
+    if (calculation.ans != undefined) {
+        calculation.number[0] = calculation.ans
+        calculation.number[1] = calculation.screen
+        calculation.screen = undefined
+        calculation.ans = undefined
+    }
+    
+    calculation.calculate()
+    calculation.archive()
+
+    mainDisplay.innerHTML = "0"
+    console.log(calculation)
 
 }
 
@@ -43,5 +74,72 @@ function utilityPressed(utility) {
 
     const deletion = ["c","C","Backspace"]
 
-    if ()
+    if (deletion.includes(utility)) mainDisplay.innerHTML = mainDisplay.innerHTML.slice(0,-1)
+    if (mainDisplay.innerHTML == '') mainDisplay.innerHTML = "0"
+}
+
+function adjustDisplay() {
+    if (mainDisplay.innerHTML.length < 15) mainDisplay.style.fontSize = "30px"
+    else mainDisplay.style.fontSize = `${360/mainDisplay.innerHTML.length + 1}px`;
+}
+
+
+window.addEventListener('keydown', (e) => captureEvent(e));
+const mainDisplay = document.querySelector(".screen-main span")
+
+const calculation = { 
+    number:[undefined,undefined],
+    add:false,
+    substract:false,
+    multiply:false,
+    divide:false,
+    ans:undefined,
+    screen:undefined,
+    history:["","","","",""],
+    calculate: function() {
+        if (this.add) this.ans = add(this.number[0],this.number[1])
+        if (this.substract) this.ans = substract(this.number[0],this.number[1])
+        if (this.multiply) this.ans = multiply(this.number[0],this.number[1])
+        if (this.divide) this.ans = divide(this.number[0],this.number[1])
+    },
+    archive: function() {
+        if (this.ans != undefined && this.number[0] != undefined && this.number[0] != undefined) {
+            if (this.add) this.history.unshift(`${this.number[0]} + ${this.number[1]} = ${calculation.ans}`)
+            if (this.substract) this.history.unshift(`${this.number[0]} - ${this.number[1]} = ${calculation.ans}`)
+            if (this.multiply) this.history.unshift(`${this.number[0]} * ${this.number[1]} = ${calculation.ans}`)
+            if (this.divide) this.history.unshift(`${this.number[0]} / ${this.number[1]} = ${calculation.ans}`)
+        }
+    },
+}
+
+
+
+
+
+
+
+
+
+
+function add(n1,n2) {
+    return n1 == undefined || n2 == undefined ? undefined : n1+n2
+}
+
+function substract(n1,n2) {
+    return n1 == undefined || n2 == undefined ? undefined : n1-n2
+}
+
+function multiply(n1,n2) {
+    return n1 == undefined || n2 == undefined ? undefined : n1*n2
+}
+
+function divide(n1,n2) {
+    return n1 == undefined || n2 == undefined ? undefined : n1/n2
+}
+
+function operatorHandler(operator) {
+    calculation.add = operator == "+" ? true:false
+    calculation.substract = operator == "-" ? true:false
+    calculation.multiply = operator == "*" ? true:false
+    calculation.divide = operator == "/" ? true:false
 }
