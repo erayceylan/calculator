3d76e0379f5f859bcfa496e6c9cf9e12dfb642d7
erayceylan <erayceylan@rocketmail.com>
2022-07-26 11:54:55 +0300

    Add function explanations

--- script.js
+++ script.js
@@ -1,15 +1,84 @@
+function add(n1,n2) {
+    // These functions are main operator functions.
+    // If one of the values is "undefined", it returns "undefined" value.
+    // They also truncate the answer to 8 digits.
+    return n1 == undefined || n2 == undefined ? undefined : +((n1+n2).toFixed(8))
+}
+function substract(n1,n2) {
+    // These functions are main operator functions.
+    // If one of the values is "undefined", it returns "undefined" value.
+    // They also truncate the answer to 8 digits.
+    return n1 == undefined || n2 == undefined ? undefined : +((n1-n2).toFixed(8))
+}
+function multiply(n1,n2) {
+    // These functions are main operator functions.
+    // If one of the values is "undefined", it returns "undefined" value.
+    // They also truncate the answer to 8 digits.
+    return n1 == undefined || n2 == undefined ? undefined : +((n1*n2).toFixed(8))
+}
+function divide(n1,n2) {
+    // These functions are main operator functions.
+    // If one of the values is "undefined", it returns "undefined" value.
+    // They also truncate the answer to 8 digits.
+    return n1 == undefined || n2 == undefined ? undefined : +((n1/n2).toFixed(8))
+}
+
+function resetCalculation() {
+
+    // When this function called it checks if an answer displayed on the screen
+    // If it is, then it removes this value and initializes "number" and "ans" attributes.
+
+    if (calculation.ansOnScreen) {
+
+        mainDisplay.innerHTML = "0"
+
+        calculation.number[0] = undefined;
+        calculation.number[1] = undefined;
+        calculation.ans = undefined;
+
+        calculation.ansOnScreen = false;
+    }
+}
+
+function adjustDisplay() {
+
+    // This function checks length of display value and sets its font size accordingly.
+
+    if (mainDisplay.innerHTML.length < 15) mainDisplay.style.fontSize = "30px"
+    else mainDisplay.style.fontSize = `${360/mainDisplay.innerHTML.length + 1}px`;
+}
+
 function Calculation() {
     
+    // This is a object creator function for Calculation object.
+    // This object stores almost all of the information about ongoing calculation.
+    // It helps to use variables about calculation globally.
+
+    // Numbers used in calculations are stored below.
+    // "number" attribute is for storing two numbers that go into operator(+,-,*,/) functions.
+    // only the numbers stored in "number" goes as an argument into operator functions.
     this.number = [undefined,undefined];
+
+    // "ans" attribute is for storing answer/return value from operator functions.
+    this.ans = undefined;
+
+    // "screen" attribute is for dynamically storing user input as a number.
+    this.screen = undefined;
+
+    // these boolean attributes are for storing active operation choice.
     this.add = false;
     this. substract = false;
     this.multiply = false;
     this.divide = false;
-    this.ans = undefined;
+
+    // "ansOnScreen" attribute is a boolean for indicating if the answer is displayed on the screen.
+    // users' number and utility input behaviours are controlled with this value.
     this.ansOnScreen = false;
-    this.screen = undefined;
+
+    // "history" attribute is an array stroing last operations as string values to display on history panel.
     this.history = ["","","","",""];
 
+    // "calculate" method checks which operation flag is up and then calls according operation funtion to calculate the answer.
     this.calculate = function() {
         if (this.add) this.ans = add(this.number[0],this.number[1])
         if (this.substract) this.ans = substract(this.number[0],this.number[1])
@@ -17,6 +86,7 @@ function Calculation() {
         if (this.divide) this.ans = divide(this.number[0],this.number[1])
     };
 
+    // "archive" method stores calculation operation. It only stores calculations where answer and numbers are valid numbers.
     this.archive = function() {
         if (this.ans != undefined && this.number[0] != undefined && this.number[0] != undefined) {
             if (this.add) this.history.unshift(`${this.number[0]} + ${this.number[1]} = ${calculation.ans}`)
@@ -27,12 +97,35 @@ function Calculation() {
     };
 }
 
+function initialize() {
+
+    // This function simply assings new "Calculation" object to calculation constant and resets the display.
+
+    calculation = new Calculation();
+    mainDisplay.innerHTML = "0"
+}
+
+function operatorHandler(operator) {
+
+    // This function sets operation function flags according to operator value.
+
+    calculation.add = operator == "+" ? true:false
+    calculation.substract = operator == "-" ? true:false
+    calculation.multiply = operator == "*" ? true:false
+    calculation.divide = operator == "/" ? true:false
+}
+
 function captureEvent(e) {
+
+    // This function is called with "keydown" input on the window.
+    // Below arrays are active key lists.
 
     const numbers = ["1","2","3","4","5","6","7","8","9","0"]
     const operators = ["+","-","*","/"]
     const equals = ["=","Enter"]
     const utilities = ["Escape",".","Backspace"]
+
+    // According to key values main functions of calculator are called.
 
     if(numbers.includes(e)) numberPressed(e)
     if(operators.includes(e)) operatorPressed(e)
@@ -42,30 +135,82 @@ function captureEvent(e) {
 
 function numberPressed(number) {
 
-    // add this functionality: if ansOnScreen true, initialize calculation
+    // When a number key is pressed this function is called.
+    // It takes number string as an input argument.
 
+    // Below function is called to check if an answer is displayed on the screen.
+    // With this function display value is changed according to desired program behaviour.
+    resetCalculation()
+
+    // As a design decison the screen displays "0" value whenever there is noting to display on the screen.
+    // When user starts to input numbers, "0" value is removed.
     if (mainDisplay.innerHTML == "0") mainDisplay.innerHTML = ''
 
+    // Input numbers are added as string values.
+    // These string values then stored as number in the "screen" attribute of calculation object.
     mainDisplay.innerHTML += number
     calculation.screen = +mainDisplay.innerHTML
 
+    // Below function sets display font size accordingly with display value length.
+    adjustDisplay()
+}
+
+function utilityPressed(utility) {
+
+    // When an utility key is pressed this function is called.
+
+    // Below function is called to check if an answer is displayed on the screen.
+    // With this function display value is changed according to desired program behaviour.
+    resetCalculation()
+
+    // These constans are utility key arrays, theys helps to easy implement new keybinds.
+    const deletion = ["Backspace"]
+    const decimal = ["."]
+    const clear = ["Escape"]
+
+    // Below line deletes last element of displayed string.
+    if (deletion.includes(utility)) mainDisplay.innerHTML = mainDisplay.innerHTML.slice(0,-1)
+
+    // Below line adds "." string to display value if none "." exists in it.
+    if (decimal.includes(utility) && !mainDisplay.innerHTML.includes(".")) mainDisplay.innerHTML += "."
+
+    // Below line calls initialize function to intialize calculation.
+    if (clear.includes(utility)) initialize()
+
+    // if (pandn.includes(utility)) mainDisplay.innerHTML = `${(+mainDisplay.innerHTML) * (-1)}`
+
+    // As a design decison the screen displays "0" value whenever there is noting to display on the screen.
+    // When user starts to input numbers, "0" value is removed.
+    if (mainDisplay.innerHTML == '') mainDisplay.innerHTML = "0"
+
+    // Below function sets display font size accordingly with display value length.
     adjustDisplay()
 }
 
 function operatorPressed(operator) {
 
+    // This is the main operator function.
+    // Storing numbers and consecutive operations behaviours are controlled with below lines.
+
+    // When an operator key pressed display screen is set to "0".
+    // In consecutive operations the result value is not displayed with operator keys.
+    // Below lines maintains this behaviour and and changes "ansOnScreen" boolean accordingly.
+    mainDisplay.innerHTML = "0"
     calculation.ansOnScreen = false
 
+    // Below lines helps to store first number argument for operator functions.
     if (calculation.number[0] == undefined) {
         calculation.number[0] = calculation.screen
         calculation.screen = undefined
     }
 
+    // Below lines helps to store second number argument for operator functions.
     if (calculation.number[1] == undefined) {
         calculation.number[1] = calculation.screen
         calculation.screen = undefined
     }
 
+    // After calculating first valid answer value, below lines starts to control number storing behaviour.
     if (calculation.ans != undefined) {
         calculation.number[0] = calculation.ans
         calculation.number[1] = calculation.screen
@@ -73,18 +218,25 @@ function operatorPressed(operator) {
         calculation.ans = undefined
     }
     
+    // After numbers stored accordingly, below functions are called to make actual operation.
     calculation.calculate()
     calculation.archive()
 
+    // This line is deep below because of correcty calculating consecutive operations.
+    // Below function sets which operator function will be used in new calculation.
     operatorHandler(operator)
 
-    mainDisplay.innerHTML = "0"
+    // This line is for debugging purposes.
     console.log(calculation)
 
 }
 
 function equalPressed() {
 
+    // This function controls the program behaviour when "Enter" or similar key is pressed.
+
+    // Below two lines of "if" statements helps the program to avoid erros when "Enter" key is pressed by the user randomly.
+    // These lines are same as the first two "if" statements of operatorPressed function.
     if (calculation.number[0] == undefined) {
         calculation.number[0] = calculation.screen
         calculation.screen = undefined
@@ -95,69 +247,39 @@ function equalPressed() {
         calculation.screen = undefined
     }
 
+    // Below lines are different from the operatorPressed function's third "if" statement.
+    // This difference helps to continute consecutive calculations when "Enter" pressed.
+    // If the user continues to press "Enter" after the first key press, the last calculation is executed repetitively. 
     if (calculation.ans != undefined) {
         calculation.number[0] = calculation.ans
         calculation.screen = undefined
         calculation.ans = undefined
     }
     
+    // After numbers stored accordingly, below functions are called to make actual operation.
     calculation.calculate()
     calculation.archive()
 
+    // When user presses "Enter" key randomly and if there is no valid answer to show,
+    // Below lines helps to show "0" zero value on the screen.
     if (calculation.ans != undefined) mainDisplay.innerHTML = `${calculation.ans}`
     else mainDisplay.innerHTML = "0"
+
+    // This boolean attribute is only switched to true in below lines.
     calculation.ansOnScreen = true
     
+    // This line is for debugging purposes.
     console.log(calculation)
 
+    // Below function sets display font size accordingly with display value length.
     adjustDisplay()
 }
 
-function utilityPressed(utility) {
-
-    // add this functionality: if ansOnScreen true, initialize calculation
-
-    const deletion = ["Backspace"]
-    const decimal = ["."]
-    const pandn = ["Escape"]
-
-    if (deletion.includes(utility)) mainDisplay.innerHTML = mainDisplay.innerHTML.slice(0,-1)
-    if (decimal.includes(utility) && !mainDisplay.innerHTML.includes(".")) mainDisplay.innerHTML += "."
-    if (pandn.includes(utility)) mainDisplay.innerHTML = `${(+mainDisplay.innerHTML) * (-1)}`
-
-    if (mainDisplay.innerHTML == '') mainDisplay.innerHTML = "0"
-
-    adjustDisplay()
-}
-
-function adjustDisplay() {
-    if (mainDisplay.innerHTML.length < 15) mainDisplay.style.fontSize = "30px"
-    else mainDisplay.style.fontSize = `${360/mainDisplay.innerHTML.length + 1}px`;
-}
-
-function add(n1,n2) {
-    return n1 == undefined || n2 == undefined ? undefined : +((n1+n2).toFixed(8))
-}
-
-function substract(n1,n2) {
-    return n1 == undefined || n2 == undefined ? undefined : +((n1-n2).toFixed(8))
-}
-
-function multiply(n1,n2) {
-    return n1 == undefined || n2 == undefined ? undefined : +((n1*n2).toFixed(8))
-}
-
-function divide(n1,n2) {
-    return n1 == undefined || n2 == undefined ? undefined : +((n1/n2).toFixed(8))
-}
-
-function operatorHandler(operator) {
-    calculation.add = operator == "+" ? true:false
-    calculation.substract = operator == "-" ? true:false
-    calculation.multiply = operator == "*" ? true:false
-    calculation.divide = operator == "/" ? true:false
-}
-
+// Event listener added to window for "keydown" action.
 window.addEventListener('keydown', (e) => captureEvent(e.key));
+
+// Main display screen is selected as an object to manipulate its value globally
 const mainDisplay = document.querySelector(".screen-main")
+
+// This line starts first calculation.
 let calculation = new Calculation()
